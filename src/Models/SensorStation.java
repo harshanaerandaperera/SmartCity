6a6f43615846e80a5975d3c3776945aea2627348
harshana perera <harshanatxn@gmail.com>
2018-07-13 02:29:05 +0530

    clean and modified SensorStation.java

--- src/Models/SensorStation.java
+++ src/Models/SensorStation.java
@@ -11,40 +11,36 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Oshin
+ * @author Harshana
  */
-public class SensorStation implements Subject,Observer{
+public class SensorStation implements Subject, Observer {
+
     String stationID;
     private ArrayList<Observer> Observers;
-    
-    public SensorStation(String stationID)
-    {
-        this.stationID=stationID;
-        Observers=new ArrayList<>();
-    }    
+
+    public SensorStation(String stationID) {
+        this.stationID = stationID;
+        Observers = new ArrayList<>();
+    }
 
     @Override
     public void registerObserver(Observer obs) {
-       Observers.add(obs);
-        throw new UnsupportedOperationException("Not supported yet.");
+        Observers.add(obs);
     }
 
     @Override
     public void unRegisterObserver(Observer obs) {
         Observers.remove(obs);
-        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     @Override
     public void Notify() {
-        
-        throw new UnsupportedOperationException("Not supported yet."); 
+
     }
 
     @Override
-    public void update(Object ob,Observer observer) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public void update(Object ob, Observer observer) {
+    
     }
-    
-    
+
 }
